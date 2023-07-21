d63b82e43457bdd5e04f18af95375c723005c57a
oguzhanihlamur <oguzhanihlamur@gmail.com>
2023-07-21 22:22:14 +0300

    İnitial commit

---
+++ quest-app-backend/src/main/java/dev/antozy/questapp/entities/User.java
@@ -0,0 +1,20 @@
+package dev.antozy.questapp.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    Long id;
+
+    String userName;
+    String password;
+
+}
